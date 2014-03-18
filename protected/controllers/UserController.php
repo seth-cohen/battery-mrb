c6d43af66f9f606d8321a6524b41b6f305883213
Seth Cohen <forwardthinking.llc@gmail.com>
2014-03-18 16:09:22 -0400

    testlab supervisor access to userAjaxserach

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 			),
 			array('allow',	
 				'actions'=>array('ajaxusersearch'),
-				'roles'=>array('manufacturing supervisor, manufactuing engineer'),
+				'roles'=>array('manufacturing supervisor, manufactuing engineer, testlab supervisor'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete','ajaxassignrole'),
